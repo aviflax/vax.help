1c66d551b7e53fd1ba83d0597721fdabec2a3b30
Avi Flax <avi@aviflax.com>
2021-03-08 23:42:32 -0500

    Fix bugs in prior merge
    Whoops!

--- src/vax/help/background.clj
+++ src/vax/help/background.clj
@@ -4,20 +4,24 @@
             [vax.help.background.email.verifications :as verifications]))
 
 (def jobs
-  [feed-updates/start
-   verifications/start])
+  {:feed-updates/start  feed-updates/start
+   :verifications/start verifications/start})
 
 (defn start
   "WARNING if an error occurs this will kill the VM!"
   [_args]
+  ;; Using println because I want to print something as soon as the function starts, just as a sign
+  ;; that something is happening, and it takes some time for the μ publisher to start up, or something.
+  (println "Starting background job runner.")
+
   (μ/start-publisher! {:type :console-json})
   
-  (μ/log ::starting :jobs jobs)
+  (μ/log ::starting :jobs (keys jobs))
   
   ;; TODO: this is simplistic! If a thread fails, we don’t restart it, and we don’t retry, and we
   ;; don’t do anything smart! We should use something smarter, at a minimum Java 5’s ExecutorService
   (try 
-    (let [threads (map #(Thread. %))]
+    (let [threads (map #(Thread. %) (vals jobs))]
       (run! #(.start %) threads)
       (run! #(.join %) threads))
     (throw (RuntimeException. "All threads seem to have exited. That’s not supposed to happen!"))
@@ -27,4 +31,5 @@
       ;; currently have the sophistication to do anything smarter than log the error and kill the
       ;; whole process. Ah well — fail fast and loud right?!
       (μ/log ::error :ex-class (.getSimpleName (class e)), :ex-msg (.getMessage e))
+      (Thread/sleep 1000)  ; give the μ publisher time to print stuff out (not SO fast, I guess)
       (System/exit 1))))
